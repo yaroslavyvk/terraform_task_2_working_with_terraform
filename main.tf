0eafb68f727577e7280cd8e2cce006c9bde7b283
Yaroslav <ya.yaroslav27@gmail.com>
2024-07-12 15:55:57 +0200

    Solution

--- main.tf
+++ main.tf
@@ -7,19 +7,30 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "task-2-rg"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = var.blob_name
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = "terraform_code.zip"
 }
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "blob_id" {
+  value = azurerm_storage_blob.example.id
+}
+
+output "blob_url" {
+  value = azurerm_storage_blob.example.url
+}
