1a0dad842a0ae25ae697729774c9e2e7a91ced48
Yaroslav <ya.yaroslav27@gmail.com>
2024-09-24 18:15:13 +0200

    Solution

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "blob_id" {
   description = "The ID of the storage blob."
-  value = azurerm_storage_blob.example.id
+  value       = azurerm_storage_blob.example.id
 }
 
 output "blob_url" {
   description = "The URL of the storage blob."
-  value = azurerm_storage_blob.example.url
+  value       = azurerm_storage_blob.example.url
 }
